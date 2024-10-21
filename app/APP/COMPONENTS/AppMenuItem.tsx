a5dc00b5b4243550cc644cdc4f5d743e38d8fd28
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-10-21 18:06:06 +0300

    Refactor AppMenuItem component and update CSS styles

--- app/APP/COMPONENTS/AppMenuItem.tsx
+++ app/APP/COMPONENTS/AppMenuItem.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 
 const AppMenuItem = ({ text, icon }: TitleIcon) => {
     return (
-        <div className='hover:bg-[#8A6032] hover:cursor-pointer text-white w-[110px] h-[35px] CENTER gap-2 rounded-md'>
+        <div className='bg-white hover:bg-[#8A6032] hover:cursor-pointer text-[#8A6032]  hover:text-white w-[110px] h-[35px] CENTER gap-2 rounded-md'>
             {icon}
             <span>{text}</span>
         </div>
