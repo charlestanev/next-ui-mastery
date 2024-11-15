ccc302017d096039dd49811f9b6f6896f77f313a
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-11-15 14:23:25 +0200

    Add MenuItemList and update Page component to render menu items dynamically

---
+++ app/APP/CONSTATNS/MenuItemList.tsx
@@ -0,0 +1,24 @@
+const MenuItemList = [
+    {
+        id: 0,
+        title: "Rottweiler",
+        link: "/Rottweiler",
+    },
+    {
+        id: 1,
+        title: "Pomeranian",
+        link: "/Pomeranian",
+    },
+    {
+        id: 2,
+        title: "Doberman",
+        link: "/Doberman",
+    },
+    {
+        id: 3,
+        title: "Husky",
+        link: "/Husky",
+    }
+];
+
+export { MenuItemList };
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import AppMenuItem from './APP/COMPONENTS/AppMenuItem'
 import { FaDog } from 'react-icons/fa'
+import { MenuItemList } from './APP/CONSTATNS/MenuItemList'
 
 
 const Page = () => {
   return (
-    <>Page
-      <AppMenuItem icon={<FaDog/>} text="breed"/>
+    <>
+      {
+        MenuItemList.map((x) => (
+          <AppMenuItem key={x.id} icon={<FaDog />} text={x.title} />
+        ))
+      }
     </>
   )
 }
