6b832ff6c8dda53edbbbad844d522d6732a6c55c
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-11-20 16:02:50 +0200

    Remove duplicate import statement in AppLeftPanel component

--- app/APP/COMPONENTS/AppLeftPanel.tsx
+++ app/APP/COMPONENTS/AppLeftPanel.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import AppMenuItemGroup from './AppMenuItemGroup'
 import Image from 'next/image'
 import { cn } from '@/lib/utils'
-import { cn } from '@/lib/utils'
 
 function AppLeftPanel() {
     return (
