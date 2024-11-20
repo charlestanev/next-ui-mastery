85ea21fdf8c1d2d40204bf2c7d9cd4209eac46e7
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-11-20 15:53:29 +0200

    Enhance AppLeftPanel layout with logo and adjust AppMenuItem height for improved styling

--- app/APP/COMPONENTS/AppLeftPanel.tsx
+++ app/APP/COMPONENTS/AppLeftPanel.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AppMenuItemGroup from './AppMenuItemGroup'
 import Image from 'next/image'
+import { cn } from '@/lib/utils'
 
 function AppLeftPanel() {
     return (
-        <div className='bg-AppSecondary h-screen w-[160px] CENTER'>
+        <div className={cn('bg-AppSecondary h-screen w-[160px] CENTER pt-10', 'flex-col gap-40 !justify-start')}>
+            <Image src='/logo.svg' width={50} height={100} alt='DogApp Logo' />
+
             <div>
-                <Image src='/logo.svg' width={100} height={100} alt='DogApp Logo' />
                 <AppMenuItemGroup />
             </div>
         </div>
