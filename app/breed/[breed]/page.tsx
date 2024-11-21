6ce287473baef81de4ee7ac0d2107f48dcf985e6
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-11-21 17:10:56 +0200

    Add back navigation link and improve layout in breed page component

--- app/breed/[breed]/page.tsx
+++ app/breed/[breed]/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { AppSlider } from '@/app/APP/COMPONENTS/AppSlider';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 import { useParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
+import { FaAngleLeft } from 'react-icons/fa'
 
 function Page() {
     const Params = useParams();
@@ -24,11 +27,27 @@ function Page() {
     }, [])
 
     return (
-        <div className='h-screen w-screen bg-AppTertiary CENTER'>
-            {
-                ImageArray ? <AppSlider ImageArrayList={ImageArray} /> : 'Loading...'
-            }
-        </div>
+
+
+        <div className='h-screen w-screen bg-AppTertiary justify-start'>
+            <div className={cn(
+                'pt-12 w-[180px] CENTER h-screen !justify-start',
+                'gap-[120px] flex-col',
+            )}>
+
+                <Link href='/'>
+                    <div className='bg-AppPop text-white CENTER w-[90px] h-[35px] rounded-md gap-2'>
+                        <FaAngleLeft /> back
+                    </div>
+                </Link>
+
+                <div>
+                    {
+                        ImageArray ? <AppSlider ImageArrayList={ImageArray} /> : 'Loading...'
+                    }
+                </div>
+            </div>
+        </div >
     )
 }
 
