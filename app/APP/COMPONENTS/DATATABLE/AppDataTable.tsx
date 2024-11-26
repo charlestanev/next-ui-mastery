e354b571448a0ce517cbf81dc9ca111328d2456a
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-11-26 16:19:40 +0200

    Add AppDataTable component with initial column definitions for payment data

---
+++ app/APP/COMPONENTS/DATATABLE/AppDataTable.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+    Avatar: number
+    ImageLink: string
+    email: string
+}
+
+export const columns: ColumnDef<Payment>[] = [
+    {
+        accessorKey: "status",
+        header: "Status",
+    },
+    {
+        accessorKey: "email",
+        header: "Email",
+    },
+    {
+        accessorKey: "amount",
+        header: "Amount",
+    },
+]
